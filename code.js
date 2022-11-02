086ccbdcaa8fe304460c01631a0eb2b19118b437
MantashaZubair <zmantasha3@gmail.com>
2022-11-02 13:31:54 -0700

    modified

--- code.js
+++ code.js
@@ -1,23 +1,45 @@
-var result = document.getElementById("result");
 
 function calculate(){
     
-    var number1= parseFloat(document.getElementById("number1").value); 
-    var number2 = parseFloat(document.getElementById("number2").value);
-    var operator=document.getElementById("operator").value;
-    if (operator === '+'){
-        result.value = number1 + number2
+    var number1= document.getElementById("number1").value; 
+    var number2 = document.getElementById("number2").value;
+    var operator =document.getElementById("operator").value;
+    var result = document.getElementById("result");
+    var num1= parseFloat(number1);
+    var num2=parseFloat(number2)
+    if(number1==""){
+        alert("please enter your number1");
+        document.getElementById("result").value="0"
+    }else if(isNaN(number1)){
+        alert("Only Numbers are allowed please check number1")
+        document.getElementById("result").value="0"
+    }else if(number2==""){
+        alert("please enter your number2");
+        document.getElementById("result").value="0"
+    }else if(isNaN(number2)){
+        alert("Only Numbers are allowed please check number2")
+        document.getElementById("result").value="0"
+    }
+    else if (operator === '+'){
+        result.value = `${number1} ${operator} ${number2} = ${num1 + num2}`
+       document.getElementById("number1").value=""; 
+         document.getElementById("number2").value="";
     }
      else if(operator=== "-"){
-        result.value = number1 - number2
+        result.value = `${number1} ${operator} ${number2} = ${num1 - num2}`
+        document.getElementById("number1").value=""; 
+        document.getElementById("number2").value="";
     }
     else if(operator=== "*"){
-        result.value = number1 * number2
+        result.value = `${number1} ${operator} ${number2} = ${num1 * num2}`
+         document.getElementById("number1").value=""; 
+         document.getElementById("number2").value="";
     }
     else if(operator=== "/"){
-        result.value = number1 / number2
+        result.value = `${number1} ${operator} ${number2} = ${num1 / num2}`
+       document.getElementById("number1").value=""; 
+        document.getElementById("number2").value="";
     }
+
 }
-function setResult() {
-    result.value = " "
-}
+
